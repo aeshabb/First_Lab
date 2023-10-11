df4cd17f9dc961845bf6a32ee2d2dfbb998650ea
aeshabb <belalesa725@gmail.com>
2023-10-11 16:56:55 +0300

    SecondLab done

--- SecondLab/src/attacks/Amnesia.java
+++ SecondLab/src/attacks/Amnesia.java
@@ -1,18 +1,17 @@
 package attacks;
 
+import ru.ifmo.se.pokemon.PhysicalMove;
 import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Stat;
-import ru.ifmo.se.pokemon.StatusMove;
 import ru.ifmo.se.pokemon.Type;
 
-public class MorningSun extends StatusMove {
-    public MorningSun() {
-        super(Type.NORMAL, 65, 100);
+public class Amnesia extends PhysicalMove {
+    public Amnesia() {
+        super(Type.PSYCHIC, 0, 0);
     }
 
     @Override
     protected void applySelfEffects(Pokemon poke) {
-        poke.setMod(Stat.HP, -2);
-
+        poke.setMod(Stat.DEFENSE, 2);
     }
 }
--- SecondLab/src/attacks/Bite.java
+++ SecondLab/src/attacks/Bite.java
@@ -5,16 +5,14 @@ import ru.ifmo.se.pokemon.PhysicalMove;
 import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Type;
 
-public class Psybeam extends PhysicalMove {
-    public Psybeam() {
-        super(Type.PSYCHIC, 65, 100);
+public class Bite extends PhysicalMove {
+    public Bite() {
+        super(Type.DARK, 60, 100);
     }
 
     @Override
     protected void applyOppEffects(Pokemon poke) {
-        if (Math.random() <= 0.1) {
-            Effect.confuse(poke);
-        }
+        if (Math.random() <= 0.3)
+            Effect.flinch(poke);
     }
-
 }
